565392abf087352db3650bfda0a17c00e7a24ffc
Manoel Silva <Manoel.Silva@wiipo.com>
2023-01-05 17:15:36 -0300

    add components Avatar, Box, Heading e Text

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,11 +1,4 @@
-import { styled } from "./styles";
-
-const Button = styled("button", {
-  fontFamily: "$default",
-  backgroundColor: "$ignite500",
-  borderRadius: "$md",
-});
-
-export function App() {
-  return <Button>Hello world</Button>;
-}
+export * from "./components/Box";
+export * from "./components/Text";
+export * from "./components/Heading";
+export * from "./components/Avatar";
