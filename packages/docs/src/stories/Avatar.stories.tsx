69685562a72540a9c88b24667e8169929b03d397
Manoel Silva <Manoel.Silva@wiipo.com>
2023-01-06 15:40:55 -0300

    add control

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,14 @@ export default {
     src: "http://github.com/manoelmeseque.png",
     alt: "Manoel Meseque",
   },
+
+  argTypes: {
+    src: {
+      control: {
+        type: "text",
+      },
+    },
+  },
 } as Meta<AvatarProps>;
 
 export const Primary: StoryObj<AvatarProps> = {};
