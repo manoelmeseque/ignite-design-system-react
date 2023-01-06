db8b701f72129a9dcca63c23a83c294b181d0a84
Manoel Silva <Manoel.Silva@wiipo.com>
2023-01-06 17:52:14 -0300

    Add /

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if(configType === "PRODUCTION"){
-      config.base = '/ignite-design-system-react'
+      config.base = '/ignite-design-system-react/'
     }
 
     return config
